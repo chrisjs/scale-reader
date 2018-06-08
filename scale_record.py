77fe30b325a493e432e4440ac0038fd0bf684cf0
Chris Schaefer <chris@dtzq.com>
2018-06-07 21:44:57 -0400

    fix calculation to get closer kilos

--- scale_record.py
+++ scale_record.py
@@ -5,6 +5,6 @@ class ScaleRecord:
         self.low_byte = low_byte
 
     def getWeight(self):
-        weight = ord(chr(self.high_byte)) * 256 + ord(chr(self.low_byte)) / 10.0
+        weight = ((ord(chr(self.high_byte)) * 256 + ord(chr(self.low_byte))) / 10.0) / 5
         return format(weight, '.1f')
 
