ff21123daf28e78771486e59da402d6cb6115457
Chris Schaefer <chris@dtzq.com>
2019-11-21 22:40:18 -0500

    update scale calibration

--- scale_record.py
+++ scale_record.py
@@ -1,8 +1,8 @@
 class ScaleRecord:
     scales = {1 : 5.15,
-              2 : 4.88,
-              3 : 5.28,
-              4 : 4.88}
+              2 : 5.43,
+              3 : 4.97,
+              4 : 5.18}
 
     def __init__(self, scale_id, high_byte, low_byte):
         self.scale_id = scale_id
@@ -12,6 +12,7 @@ class ScaleRecord:
     def getWeight(self):
         weight = ((ord(chr(self.high_byte)) * 256 + ord(chr(self.low_byte))) / 10.0)
         weight = weight / self.scales[self.scale_id]
+        weight = weight * 2.204623
 
         return format(weight, '.1f')
 
